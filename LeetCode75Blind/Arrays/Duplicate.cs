f655f3a931cf66f6032343fa6ae5a2543c6114a7
amolankit <amolankit@live.com>
2023-11-14 22:41:27 +0000

    CotainsDuplicate class

---
+++ LeetCode75Blind/Arrays/Duplicate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace LeetCode75Blind.Arrays
+{
+	public class Duplicate
+	{
+        public bool ContainsDuplicate(int[] nums)
+        {
+            Array.Sort(nums);
+            for (int i = 0; i < nums.Length-1; i++)
+            {
+                if (nums[i] == nums[i + 1])
+                    return true;
+            }
+            return false;
+        }
+    }
+}
+
