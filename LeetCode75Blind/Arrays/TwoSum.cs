2be60754a420e148da02cff34c8ddfa89a28026e
amolankit <amolankit@live.com>
2023-11-14 22:35:33 +0000

    Created solution for Two Sum solution

---
+++ LeetCode75Blind/Arrays/TwoSum.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace LeetCode75Blind.Arrays
+{
+	public class ArrayTwoSum
+	{
+        public int[] TwoSum(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        return new int[2] { i, j };
+                    }
+                }
+            }
+            return null;
+        }
+    }
+}
+
